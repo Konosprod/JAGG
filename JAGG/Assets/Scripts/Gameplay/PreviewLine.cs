c52f3efbdede58bfbeb4ee9b0f62ad1486474a68
WOLFROM Matthieu <keithdae@gmail.com>
2017-10-20 17:12:47 +0200

    Preview line added, physics upgraded, hole collider fixed

---
+++ JAGG/Assets/Scripts/Gameplay/PreviewLine.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PreviewLine : MonoBehaviour {
+
+    public Rigidbody rb;
+    public Camera cam;
+    public LineRenderer line;
+
+	// Use this for initialization
+	void Start ()
+    {
+        if (rb == null)
+            Debug.Log("PAS DE RIGIDBODY WTF BRO");
+        if (cam == null)
+            Debug.Log("CAMERA STP");
+        if (line == null)
+            Debug.Log("ELLE EST OU LA LINE ???");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (rb != null && cam != null && line != null)
+        {
+            if (Mathf.Approximately(rb.velocity.magnitude, 0f))
+            {
+                if (!line.enabled)
+                    line.enabled = true;
+
+                Vector3 dir = transform.position - cam.transform.position;
+                dir = new Vector3(dir.x, 0f, dir.z).normalized;
+                line.SetPosition(0, transform.position);
+                line.SetPosition(1, dir / 1.3f + transform.position);
+            }
+            else
+            {
+                line.enabled = false;
+            }
+        }
+	}
+}
---
+++ JAGG/Assets/Scripts/Gameplay/ShootBall.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootBall : MonoBehaviour {
+
+    public Rigidbody rb;
+    public Camera cam;
+
+    public float minForce = 100f;
+    public float force = 400f;
+    public float maxForce = 1500f;
+
+	// Use this for initialization
+	void Start () {
+        if (rb == null)
+            Debug.Log("PAS DE RIGIDBODY WTF BRO");
+        if (cam == null)
+            Debug.Log("CAMERA STP");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (cam != null && rb != null)
+        {
+            if (Mathf.Approximately(rb.velocity.magnitude,0f))
+            {
+                Vector3 dir = transform.position - cam.transform.position;
+                dir = new Vector3(dir.x, 0f, dir.z).normalized;
+                if (Input.GetMouseButtonDown(0))
+                {
+                    rb.AddForce(dir * force);
+                    Debug.Log("dir = " + dir.ToString() + ", ball pos = " + transform.position.ToString() + ", cam pos = " + cam.transform.position.ToString());
+                }
+            }
+        }
+	}
+}
