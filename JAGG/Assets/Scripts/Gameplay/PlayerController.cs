b0012d8ec4c72c8507670e72e7717e2fbb90d41f
Konosprod <konosprod@free.fr>
2017-10-23 12:11:45 +0200

    Add level properties, timer, and max shot logic
    Signed-off-by: Konosprod <konosprod@free.fr>

--- JAGG/Assets/Scripts/Gameplay/PlayerController.cs
+++ JAGG/Assets/Scripts/Gameplay/PlayerController.cs
@@ -2,15 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine.Networking;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : NetworkBehaviour {
 
     public float force;
+    public bool canShoot = true;
+    public int shots = 0;
+    public Text timerText;
+
+    private float timer = 0f;
+    private bool isStarted = false;
+    LevelProperties levelProperties;
 
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Confined;
         Cursor.visible = false;
+
+
+        //Find better way to do this
+        levelProperties = GameObject.Find("Level Properties").GetComponent<LevelProperties>();
+        timerText = GameObject.Find("TimerText").GetComponent<Text>();
+
+        this.timer = levelProperties.maxTime;
     }
 
     private void Update()
@@ -20,16 +35,34 @@ public class PlayerController : NetworkBehaviour {
             return;
         }
 
+        //Debug.Log("Started : " + isStarted.ToString() + " Timer : " + timer.ToString());
+
         Rigidbody rb = GetComponent<Rigidbody>();
 
         if (Mathf.Approximately(rb.velocity.magnitude, 0f))
         {
             Vector3 dir = transform.position - Camera.main.transform.position;
             dir = new Vector3(dir.x, 0f, dir.z).normalized;
-            if (Input.GetMouseButtonDown(0))
+
+            if (Input.GetMouseButtonDown(0) && canShoot)
             {
-                rb.AddForce(dir * force);
-                Debug.Log("dir = " + dir.ToString() + ", ball pos = " + transform.position.ToString() + ", cam pos = " + Camera.main.transform.position.ToString());
+                Shoot(dir, rb);
+            }
+        }
+
+        if (isStarted)
+        {
+            timer -= Time.deltaTime;
+
+            if (timer > 0)
+            {
+                timerText.text = timer.ToString("Time: 0 s");
+            }
+            else
+            {
+                //send timer finished to the server
+
+                DisablePlayer();
             }
         }
     }
@@ -38,6 +71,31 @@ public class PlayerController : NetworkBehaviour {
     {
         Camera.main.GetComponent<BallCamera>().target = GetComponent<Transform>();
         GetComponent<PreviewLine>().enabled = true;
+
+        isStarted = true;
+
     }
 
+    public void Shoot(Vector3 dir, Rigidbody rb)
+    {
+        rb.AddForce(dir * force);
+        Debug.Log("dir = " + dir.ToString() + ", ball pos = " + transform.position.ToString() + ", cam pos = " + Camera.main.transform.position.ToString());
+
+        shots++;
+
+        if(shots >= levelProperties.maxShot)
+        {
+            //send max shot reached
+
+            DisablePlayer();
+        }
+    }
+
+    private void DisablePlayer()
+    {
+        canShoot = false;
+        GetComponent<PreviewLine>().enabled = false;
+        GetComponent<LineRenderer>().enabled = false;
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+    }
 }
---
+++ JAGG/Assets/Scripts/LevelProperties.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LevelProperties : NetworkBehaviour {
+
+    public string NextLevel;
+    public int par;
+    public int maxShot;
+    public float maxTime;
+}
