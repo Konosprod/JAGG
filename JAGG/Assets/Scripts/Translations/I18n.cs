f302ecfabb0fb76cfc0bf8db1eaceb9bbf18367a
Konosprod <konosprod@free.fr>
2017-11-04 10:52:11 +0100

    Add transalation utilities
    Signed-off-by: Konosprod <konosprod@free.fr>

---
+++ JAGG/Assets/Scripts/Translations/I18n.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mgl;
+
+namespace JAGG
+{
+    public class I18n : Mgl.I18n
+    {
+        new protected static readonly I18n instance = new I18n();
+
+        new protected static string[] locales = new string[] {
+            "en-US",
+            "fr-FR"
+        };
+
+        new public static I18n Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+    }
+}
---
+++ JAGG/Assets/Scripts/Translations/TextTranslator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JAGG;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextTranslator : MonoBehaviour {
+
+    private I18n i18n = I18n.Instance;
+
+    public string text;
+
+	// Use this for initialization
+	void Start() {
+        Mgl.I18n.SetLocale("fr-FR");
+        GetComponent<Text>().text = i18n.__(text);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
