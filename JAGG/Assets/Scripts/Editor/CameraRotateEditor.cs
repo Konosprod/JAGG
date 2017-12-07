6d8fc338d89f650a98e065fc25d52b48dc65805a
Konosprod <konosprod@free.fr>
2017-12-07 17:44:28 +0100

    Create rotation script & custom editor for the lulz
    Signed-off-by: Konosprod <konosprod@free.fr>

---
+++ JAGG/Assets/Scripts/Editor/CameraRotateEditor.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(RotateCamera))]
+[CanEditMultipleObjects]
+public class RotateCameraEditor : Editor
+{
+    void OnEnable()
+    {
+
+    }
+
+    public override void OnInspectorGUI()
+    {
+        RotateCamera rotateCamera = target as RotateCamera;
+
+        serializedObject.Update();
+        rotateCamera.angle = EditorGUILayout.FloatField("Angle", rotateCamera.angle);
+        rotateCamera.speed = EditorGUILayout.FloatField("Speed", rotateCamera.speed);
+
+        rotateCamera.onPoint = EditorGUILayout.Toggle("On Point", rotateCamera.onPoint);
+
+        if (rotateCamera.onPoint)
+        {
+            rotateCamera.point = EditorGUILayout.Vector3Field("Point", rotateCamera.point);
+        }
+        else
+        {
+            rotateCamera.target = EditorGUILayout.ObjectField("Target", rotateCamera.target, typeof(GameObject), true) as GameObject;
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
