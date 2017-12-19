57b3e7137471602c11b11ccf18388adb6ba5c4ee
Konosprod <konosprod@free.fr>
2017-12-19 11:10:20 +0100

    Fix layout not calculated when returning to the lobby for client
    Signed-off-by: Konosprod <konosprod@free.fr>

--- JAGG/Assets/Scripts/LobbyPlayerList.cs
+++ JAGG/Assets/Scripts/LobbyPlayerList.cs
@@ -1,33 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class LobbyPlayerList : MonoBehaviour {
+public class LobbyPlayerList : MonoBehaviour
+{
 
-        public static LobbyPlayerList _instance = null;
+    public static LobbyPlayerList _instance = null;
 
-        public RectTransform playerListContentTransform;
+    public RectTransform playerListContentTransform;
 
-        protected List<LobbyPlayer> _players = new List<LobbyPlayer>();
+    protected List<LobbyPlayer> _players = new List<LobbyPlayer>();
+    protected HorizontalLayoutGroup _layout;
 
-        public void OnEnable()
-        {
-            _instance = this;
-        }
+    public void OnEnable()
+    {
+        _instance = this;
+        _layout = playerListContentTransform.GetComponent<HorizontalLayoutGroup>();
+    }
 
-        public void AddPlayer(LobbyPlayer player)
-        {
-            if (_players.Contains(player))
-                return;
+    void Update()
+    {
+        //this dirty the layout to force it to recompute evryframe (a sync problem between client/server
+        //sometime to child being assigned before layout was enabled/init, leading to broken layouting)
 
-            _players.Add(player);
+        if (_layout)
+            _layout.childAlignment = Time.frameCount % 2 == 0 ? TextAnchor.UpperCenter : TextAnchor.UpperLeft;
+    }
 
-            player.transform.SetParent(playerListContentTransform, false);
-        }
 
-        public void RemovePlayer(LobbyPlayer player)
-        {
-            _players.Remove(player);
-        }
+    public void AddPlayer(LobbyPlayer player)
+    {
+        if (_players.Contains(player))
+            return;
+
+        _players.Add(player);
+
+        player.transform.SetParent(playerListContentTransform, false);
+    }
+
+    public void RemovePlayer(LobbyPlayer player)
+    {
+        _players.Remove(player);
+    }
 
 }
