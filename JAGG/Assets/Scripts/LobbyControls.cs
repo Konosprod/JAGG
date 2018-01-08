9ee84cff46b1b86e32ee547c5e99db2de2735cda
Konosprod <konosprod@free.fr>
2018-01-08 16:07:56 +0100

    Create level seclection window
    Signed-off-by: Konosprod <konosprod@free.fr>

---
+++ JAGG/Assets/Scripts/LobbyControls.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class LobbyControls : MonoBehaviour {
+
+    public Transform contentPanel;
+    public GameObject prefabButton;
+
+
+    public Button selectButton;
+    public Text levelName;
+    public Text lobbyLevelName;
+
+    public string selectedScene;
+
+    public LobbyManager lobbyManager;
+
+    void Start()
+    {
+        ListScenes();
+    }
+
+    void OnEnable()
+    {
+        selectButton.onClick.RemoveAllListeners();
+        selectButton.onClick.AddListener(SetSelectedScene);
+    }
+
+    public void ListScenes()
+    {
+        int sceneCount = SceneManager.sceneCountInBuildSettings;
+
+        for(int i = 0; i < sceneCount; i++)
+        {
+            string path = SceneUtility.GetScenePathByBuildIndex(i);
+
+            if(path.StartsWith("Assets/Scenes/Levels"))
+            {
+                GameObject newButton = GameObject.Instantiate(prefabButton, contentPanel);
+
+                SceneListEntry entry = newButton.GetComponent<SceneListEntry>();
+                entry.SetUp(System.IO.Path.GetFileNameWithoutExtension(path), levelName, this);
+            }
+        }
+    }
+
+    public void SetSelectedScene()
+    {
+        lobbyManager.playScene = selectedScene;
+        lobbyLevelName.text = selectedScene;
+    }
+}
---
+++ JAGG/Assets/Scripts/SceneListEntry.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class SceneListEntry : MonoBehaviour {
+
+    public Text labelName;
+    public Button buttonEntry;
+
+    public Text labelTitle;
+
+    public LobbyControls lobbyControls;
+
+	// Use this for initialization
+	void Start() {
+        buttonEntry.onClick.RemoveAllListeners();
+        buttonEntry.onClick.AddListener(Selected);
+	}
+	
+
+    public void SetUp(string name, Text levelName, LobbyControls lobbyControls)
+    {
+        labelName.text = name;
+        labelTitle = levelName;
+        this.lobbyControls = lobbyControls;
+    }
+
+    public void Selected()
+    {
+        labelTitle.text = labelName.text;
+
+        lobbyControls.selectedScene = labelName.text;
+    }
+}
