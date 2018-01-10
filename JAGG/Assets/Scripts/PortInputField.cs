d9d92da06ee50cd0a76b5fac5ae60889e7043dad
Konosprod <konosprod@free.fr>
2018-01-10 15:58:15 +0100

    Add custom content-type for ip input and port input, allow character in only available in ip and port, something like user input verification
    Signed-off-by: Konosprod <konosprod@free.fr>

---
+++ JAGG/Assets/Scripts/PortInputField.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using System.Text.RegularExpressions;
+
+public class PortInputField : MonoBehaviour {
+
+    public InputField inputField;
+
+    private string allowed = "[^0-9]";
+
+    public void UpdateField()
+    {
+        string text = inputField.text;
+        string output = Regex.Replace(text, allowed, string.Empty, RegexOptions.IgnoreCase);
+        inputField.text = output;
+
+    }
+}
