f45aae6a76c658dca8a7a8d67f5136b4bbc64d10
Konosprod <konosprod@free.fr>
2018-01-12 10:50:09 +0100

    Enable create/join only if inputs are really valid
    Signed-off-by: Konosprod <konosprod@free.fr>

--- JAGG/Assets/Scripts/UI/IPInputField.cs
+++ JAGG/Assets/Scripts/UI/IPInputField.cs
@@ -6,7 +6,16 @@ public class IPInputField : MonoBehaviour {
 
     public InputField inputField;
 
+    [HideInInspector]
+    public bool isValid = false;
+
     private string allowed = "[^0-9.]";
+
+    void Start()
+    {
+        isValid = false;
+        UpdateField();
+    }
 
     public void UpdateField()
     {
@@ -14,5 +23,38 @@ public class IPInputField : MonoBehaviour {
         string output = Regex.Replace(text, allowed, string.Empty, RegexOptions.IgnoreCase);
         inputField.text = output;
 
+        ValidateIPv4(output);
+    }
+
+    public void ValidateIPv4(string ip)
+    {
+        string[] bytes = ip.Split('.');
+
+        if(bytes.Length == 4)
+        {
+            foreach(string b in bytes)
+            {
+                if (b != "")
+                {
+                    byte res = 0;
+                    if (!byte.TryParse(b, out res))
+                    {
+                        isValid = false;
+                        return;
+                    }
+                }
+                else
+                {
+                    isValid = false;
+                    return;
+                }
+            }
+
+            isValid = true;
+        }
+        else
+        {
+            isValid = false;
+        }
     }
 }
