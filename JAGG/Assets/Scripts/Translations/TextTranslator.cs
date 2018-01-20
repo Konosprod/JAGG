235e15dde64ac3debd1e171563ce169aa02cb023
Konosprod <konosprod@free.fr>
2018-01-20 14:05:32 +0100

    New font, optioons menu has now a scrollview, update translations
    Signed-off-by: Konosprod <konosprod@free.fr>

--- JAGG/Assets/Scripts/Translations/TextTranslator.cs
+++ JAGG/Assets/Scripts/Translations/TextTranslator.cs
@@ -8,12 +8,15 @@ public class TextTranslator : MonoBehaviour {
 
     private I18n i18n = I18n.Instance;
 
-    public string text;
+    private Text textEntry;
 
 	// Use this for initialization
 	void Start() {
+        textEntry = GetComponent<Text>();
+
         Mgl.I18n.SetLocale("fr-FR");
-        GetComponent<Text>().text = i18n.__(text);
+        string text = textEntry.text;
+        textEntry.text = i18n.__(text);
     }
 	
 	// Update is called once per frame
