3b72a7745b16a65cd1384dad0febceacd4d32b3d
Konosprod <konosprod@free.fr>
2018-02-08 15:38:41 +0100

    Filesaver, update fileEntry, we can now scroll even if the mouse isn't on the scrollbar. Update color, better UI
    Signed-off-by: Konosprod <konosprod@free.fr>

---
+++ JAGG/Assets/Scripts/UI/OverwriteMessageBox.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class OverwriteMessageBox : MonoBehaviour {
+
+    public FileSaver fileSaver;
+    public Text filenameText;
+    public Button yesButton;
+    public Button noButton;
+
+    [HideInInspector]
+    public string path;
+
+	// Use this for initialization
+	void Start () {
+
+        yesButton.onClick.AddListener(delegate ()
+        {
+            gameObject.SetActive(false);
+            fileSaver.SaveObject(path, true);
+        });
+
+        noButton.onClick.AddListener(delegate ()
+        {
+            gameObject.SetActive(false);
+            fileSaver.EnableButtons();
+        });
+
+
+        filenameText.text = path;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
