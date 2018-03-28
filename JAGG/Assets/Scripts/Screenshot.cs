3aaa2b8f58963f49fbd4d556aff520ddbd1ab3ad
Konosprod <konosprod@free.fr>
2018-03-28 11:30:46 +0200

    Create screenshot utility
    Signed-off-by: Konosprod <konosprod@free.fr>

---
+++ JAGG/Assets/Scripts/Screenshot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Screenshot : MonoBehaviour {
+
+    public Image image;
+
+    public void TakeScreenShot()
+    {
+        Texture2D texture = ScreenCapture.CaptureScreenshotAsTexture();
+
+        texture.Apply();
+        image.sprite = Sprite.Create(texture, new Rect(0.0f, 0.0f, texture.width, texture.height), new Vector2(0f, 0f));
+    }
+}
