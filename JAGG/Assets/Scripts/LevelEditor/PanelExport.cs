e17e52a12b4bb6a7cd7656d0bd6895669a86a164
Konosprod <konosprod@free.fr>
2018-03-30 12:05:08 +0200

    GUI to upload map to the server, not implemented yet, but no merge in the future
    Signed-off-by: Konosprod <konosprod@free.fr>

---
+++ JAGG/Assets/Scripts/LevelEditor/PanelExport.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelExport : MonoBehaviour {
+
+    public Image imagePreview;
+    public InputField tagsInput;
+    public InputField nameInput;
+    public Button saveLocalButton;
+    public Button uploadButton;
+
+	// Use this for initialization
+	void Start () {
+        uploadButton.onClick.AddListener(UploadMap);
+        saveLocalButton.onClick.AddListener(SaveLocal);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void UploadMap()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    public void SaveLocal()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
