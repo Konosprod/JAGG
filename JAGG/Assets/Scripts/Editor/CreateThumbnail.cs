0c118d16e3d5bae3115262c11e5827c995ab880d
Konosprod <konosprod@free.fr>
2018-04-17 10:25:27 +0200

    Fix TreeFir position, add grass
    Signed-off-by: Konosprod <konosprod@free.fr>

--- JAGG/Assets/Scripts/Editor/CreateThumbnail.cs
+++ JAGG/Assets/Scripts/Editor/CreateThumbnail.cs
@@ -11,22 +11,25 @@ public class CreateThumbnail : Editor {
     private static void CreatePreviews()
     {
         GameObject[] prefabs = Resources.LoadAll<GameObject>("Prefabs/Terrain");
+
         foreach (GameObject pref in prefabs)
         {
-            Debug.Log("Generating : " + pref.name); 
             string path = "Resources/Previews/" + pref.name + "Preview.png";
-            Texture2D prev = AssetPreview.GetAssetPreview(pref);
-            Texture2D prev_copy = new Texture2D(prev.width, prev.height, prev.format, prev.mipmapCount > 1);
-            prev_copy.LoadRawTextureData(prev.GetRawTextureData());
-            byte[] b_prev = prev_copy.EncodeToPNG();
+            if (Resources.Load(path) == null)
+            {
+                Texture2D prev = AssetPreview.GetAssetPreview(pref);
+                Texture2D prev_copy = new Texture2D(prev.width, prev.height, prev.format, prev.mipmapCount > 1);
+                prev_copy.LoadRawTextureData(prev.GetRawTextureData());
+                byte[] b_prev = prev_copy.EncodeToPNG();
 
-            File.WriteAllBytes(Application.dataPath+"/"+ path, b_prev);
-            AssetDatabase.Refresh();
-            AssetDatabase.ImportAsset("Assets/" + path);
+                File.WriteAllBytes(Application.dataPath + "/" + path, b_prev);
+                AssetDatabase.Refresh();
+                AssetDatabase.ImportAsset("Assets/" + path);
 
-            TextureImporter importer = AssetImporter.GetAtPath("Assets/"+path) as TextureImporter;
-            importer.textureType = TextureImporterType.Sprite;
-            importer.SaveAndReimport();
+                TextureImporter importer = AssetImporter.GetAtPath("Assets/" + path) as TextureImporter;
+                importer.textureType = TextureImporterType.Sprite;
+                importer.SaveAndReimport();
+            }
 
         }
     }
