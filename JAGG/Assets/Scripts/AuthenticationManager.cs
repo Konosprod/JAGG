9d6911b4c05c95206453dd6192612d21d2bcd21b
Konosprod <konosprod@free.fr>
2018-05-28 09:49:16 +0200

    Add retry count to authentication, shows contact mail in case of error close #78
    Signed-off-by: Konosprod <konosprod@free.fr>

--- JAGG/Assets/Scripts/AuthenticationManager.cs
+++ JAGG/Assets/Scripts/AuthenticationManager.cs
@@ -11,12 +11,13 @@ public class AuthenticationManager : MonoBehaviour {
 
     public bool isAuthenticated;
     public LoadingOverlay loadingOverlay;
+    public int maxRetry = 3;
 
     public string sessionCookie = "";
 
     public static AuthenticationManager _instance;
 
-    private bool test = false;
+    private int nbRetry = 0;
 
     void Awake()
     {
@@ -76,51 +77,61 @@ public class AuthenticationManager : MonoBehaviour {
 
     IEnumerator Authenticate()
     {
-        byte[] ticket = new byte[1024];
-        uint ticketSize = 0;
-        SteamUser.GetAuthSessionTicket(ticket, 1000, out ticketSize);
-
-        loadingOverlay.messageText.text = "Authentification...";
-
-        WWWForm form = new WWWForm();
-        form.AddField("ticket", ByteArrayToString(ticket));
-        form.AddField("steamid", (SteamUser.GetSteamID().m_SteamID).ToString());
-
-        UnityWebRequest www = UnityWebRequest.Post("https://jagg.konosprod.fr/api/auth", form);
-        www.SetRequestHeader("Cookie", sessionCookie);
-        www.SetRequestHeader("User-Agent", @"Mozilla / 5.0(Android 4.4; Mobile; rv: 41.0) Gecko / 41.0 Firefox / 41.0");
-
-        yield return www.SendWebRequest();
-
-        if (www.isNetworkError || www.isHttpError)
+        if (nbRetry >= maxRetry)
         {
-            Debug.Log(www.error);
-            Debug.Log(www.responseCode);
-            Debug.Log(www.downloadHandler.text);
-            loadingOverlay.messageText.text = "Error, retrying in 3 secs";
-            yield return new WaitForSeconds(3);
-            StartCoroutine(Authenticate());
+            loadingOverlay.messageText.text = "Error, contact us at : jagg@konosprod.fr";
+            yield return null;
         }
         else
         {
-            JSONNode node = JSON.Parse(www.downloadHandler.text);
+            byte[] ticket = new byte[1024];
+            uint ticketSize = 0;
+            SteamUser.GetAuthSessionTicket(ticket, 1000, out ticketSize);
 
-            if (node["auth"].AsBool)
+            loadingOverlay.messageText.text = "Authentification...";
+
+            WWWForm form = new WWWForm();
+            form.AddField("ticket", ByteArrayToString(ticket));
+            form.AddField("steamid", (SteamUser.GetSteamID().m_SteamID).ToString());
+
+            UnityWebRequest www = UnityWebRequest.Post("https://jagg.konosprod.fr/api/auth", form);
+            www.SetRequestHeader("Cookie", sessionCookie);
+            www.SetRequestHeader("User-Agent", @"Mozilla / 5.0(Android 4.4; Mobile; rv: 41.0) Gecko / 41.0 Firefox / 41.0");
+
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
             {
-                sessionCookie = www.GetResponseHeader("Set-Cookie");
-                isAuthenticated = true;
-                loadingOverlay.StopAnimation();
-                loadingOverlay.gameObject.SetActive(false);
+                Debug.Log(www.error);
+                Debug.Log(www.responseCode);
+                Debug.Log(www.downloadHandler.text);
+                loadingOverlay.messageText.text = "Error, retrying in 3 secs" + nbRetry.ToString() + "/" + maxRetry.ToString();
+                nbRetry++;
+                yield return new WaitForSeconds(3);
+                StartCoroutine(Authenticate());
             }
             else
             {
-                loadingOverlay.messageText.text = "Error, retrying in 3 secs";
-                Debug.Log("error while authenticating");
-                isAuthenticated = false;
-                loadingOverlay.StopAnimation();
-                loadingOverlay.gameObject.SetActive(false);
-                yield return new WaitForSeconds(3);
-                StartCoroutine(Authenticate());
+                JSONNode node = JSON.Parse(www.downloadHandler.text);
+
+                if (node["auth"].AsBool)
+                {
+                    sessionCookie = www.GetResponseHeader("Set-Cookie");
+                    isAuthenticated = true;
+                    loadingOverlay.StopAnimation();
+                    loadingOverlay.gameObject.SetActive(false);
+                }
+                else
+                {
+                    loadingOverlay.messageText.text = "Error, retrying in 3 secs" + nbRetry.ToString() + "/" + maxRetry.ToString();
+                    Debug.Log("error while authenticating");
+                    isAuthenticated = false;
+                    loadingOverlay.StopAnimation();
+                    loadingOverlay.gameObject.SetActive(false);
+                    nbRetry++;
+                    yield return new WaitForSeconds(3);
+                    StartCoroutine(Authenticate());
+                }
             }
         }
 
